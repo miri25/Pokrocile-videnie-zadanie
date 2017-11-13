e828ec18297be6298a7ec5f0786ba5147cb97383
Miroslav Gajdzik <miroslav.gajdzik@student.tuke.sk>
2017-11-13 21:01:43 +0100

    - pridanie projektu na git - pridanie testovacej metody na otvaranie obrazkov

---
+++ PV2_zadanie/PV2_zadanie/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.UI;
+
+namespace PV2_zadanie
+{
+    public partial class Form1 : Form
+    {
+        private Image<Bgr, byte> img;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // test open image
+        private void imageBox1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    img = new Image<Bgr, byte>(ofd.FileName);
+                    imageBox1.Image = img;
+                }
+            }
+        }
+    }
+}
