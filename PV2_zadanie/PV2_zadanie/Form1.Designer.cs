bc3505a2d08cbad0091cc9d7c44c26e0142ce5fe
Miroslav Gajdzik <miroslav.gajdzik@student.tuke.sk>
2017-11-13 21:37:59 +0100

    - pridanie imageBoxu, tlačidla a potrebného kódu pre zobrazenie záznamu z webkamery

--- PV2_zadanie/PV2_zadanie/Form1.Designer.cs
+++ PV2_zadanie/PV2_zadanie/Form1.Designer.cs
@@ -30,7 +30,10 @@
         {
             this.components = new System.ComponentModel.Container();
             this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.captureButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // imageBox1
@@ -44,15 +47,38 @@
             this.imageBox1.TabStop = false;
             this.imageBox1.Click += new System.EventHandler(this.imageBox1_Click);
             // 
+            // imageBox2
+            // 
+            this.imageBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox2.Location = new System.Drawing.Point(705, 21);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(534, 333);
+            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
+            // 
+            // captureButton
+            // 
+            this.captureButton.Location = new System.Drawing.Point(776, 391);
+            this.captureButton.Name = "captureButton";
+            this.captureButton.Size = new System.Drawing.Size(156, 50);
+            this.captureButton.TabIndex = 4;
+            this.captureButton.Text = "Start capture";
+            this.captureButton.UseVisualStyleBackColor = true;
+            this.captureButton.Click += new System.EventHandler(this.captureButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1006, 583);
+            this.ClientSize = new System.Drawing.Size(1337, 527);
+            this.Controls.Add(this.captureButton);
+            this.Controls.Add(this.imageBox2);
             this.Controls.Add(this.imageBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -60,6 +86,8 @@
         #endregion
 
         private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button captureButton;
     }
 }
 
--- PV2_zadanie/PV2_zadanie/Form1.cs
+++ PV2_zadanie/PV2_zadanie/Form1.cs
@@ -11,16 +11,60 @@ using Emgu.CV;
 using Emgu.CV.Structure;
 using Emgu.Util;
 using Emgu.CV.UI;
+using Emgu.CV.CvEnum;
 
 namespace PV2_zadanie
 {
     public partial class Form1 : Form
     {
         private Image<Bgr, byte> img;
+        private VideoCapture _capture = null;
+        private bool _captureInProgress;
+        private Mat _frame;
+        //private Mat _grayFrame;
+        //private Mat _smallGrayFrame;
+        //private Mat _smoothedGrayFrame;
+        //private Mat _cannyFrame;
 
         public Form1()
         {
             InitializeComponent();
+            CvInvoke.UseOpenCL = false;
+            try
+            {
+                _capture = new VideoCapture();
+                _capture.ImageGrabbed += ProcessFrame;
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+            _frame = new Mat();
+            //_grayFrame = new Mat();
+            //_smallGrayFrame = new Mat();
+            //_smoothedGrayFrame = new Mat();
+            //_cannyFrame = new Mat();
+        }
+
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            if (_capture != null && _capture.Ptr != IntPtr.Zero)
+            {
+                _capture.Retrieve(_frame, 0);
+
+                //CvInvoke.CvtColor(_frame, _grayFrame, ColorConversion.Bgr2Gray);
+
+                //CvInvoke.PyrDown(_grayFrame, _smallGrayFrame);
+
+                //CvInvoke.PyrUp(_smallGrayFrame, _smoothedGrayFrame);
+
+                //CvInvoke.Canny(_smoothedGrayFrame, _cannyFrame, 100, 60);
+
+                imageBox2.Image = _frame;
+                //grayscaleImageBox.Image = _grayFrame;
+                //smoothedGrayscaleImageBox.Image = _smoothedGrayFrame;
+                //cannyImageBox.Image = _cannyFrame;
+            }
         }
 
         // test open image
@@ -35,5 +79,33 @@ namespace PV2_zadanie
                 }
             }
         }
+
+        private void captureButton_Click(object sender, EventArgs e)
+        {
+            if (_capture != null)
+            {
+                if (_captureInProgress)
+                {  //stop the capture
+                    captureButton.Text = "Start Capture";
+                    _capture.Pause();
+                }
+                else
+                {
+                    //start the capture
+                    captureButton.Text = "Stop";
+                    _capture.Start();
+                }
+
+                _captureInProgress = !_captureInProgress;
+            }
+        }
+
+        //private void ReleaseData()
+        //{
+        //    if (_capture != null)
+        //        _capture.Dispose();
+        //}
+
+
     }
 }
